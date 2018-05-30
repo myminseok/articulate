3ff925c204b78b22425903df52eaaf988c5cd40d
MinSeok Kim <myminseok@gmail.com>
2018-05-30 19:30:19 +0900

    add-mvn-repo

--- mvn-generatescript.sh
+++ mvn-generatescript.sh
@@ -8,14 +8,12 @@ username="admin"
 password="admin123"
 releaseurl="http://10.10.10.199:8081/nexus/content/repositories/releases/"
 
-find ./mvn-dependency -name '*.*' -type f | cut -c 18-  > $jarfiles
+find ./mvn-repository -name '*.*' -type f | cut -c 18-  > $jarfiles
 
 echo "" > $uploadscript
 while read jarpath; do
-  
-   artifactpath=`find ~/.m2/repository -name $jarpath | awk '{split($0,a,".m2/repository/"); print a[2]}'`
-   echo "upload $artifactpath to $releaseurl"
-   echo "curl -v -u $username:$password --upload-file ./mvn-dependency/$jarpath $releaseurl$artifactpath" >> $uploadscript
+   echo "upload $jarpath to $releaseurl"
+   echo "curl -v -u $username:$password --upload-file ./mvn-repository/$jarpath $releaseurl$jarpath" >> $uploadscript
 
 done <$jarfiles
 
